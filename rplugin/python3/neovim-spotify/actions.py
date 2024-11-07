365bb62c6fda2a15e49b8e304495d9e502d7c618
AvivKermann <avivkermann@gmail.com>
2024-11-07 18:22:00 +0200

    reverting

--- rplugin/python3/neovim-spotify/actions.py
+++
@@ -1,9 +0,0 @@
-from .command import NeovimSpotify
-import subprocess
-
-def get_currently_playing_track(self: NeovimSpotify):
-    response = subprocess.run(["spt", "playback", "-s", "-f", "%t by %a"])
-    if response.returncode != 0:
-        self.nvim.command("echo 'Error fetching current track'")
-
-    self.nvim.command(f"echo '{response.stdout.decode().strip()}'")

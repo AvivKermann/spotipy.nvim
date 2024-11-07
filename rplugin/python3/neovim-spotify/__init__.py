9e3f2e9a0241d62a3b263bcb169b3606e36eca1d
AvivKermann <avivkermann@gmail.com>
2024-11-07 17:50:09 +0200

    checking

--- rplugin/python3/neovim-spotify/__init__.py
+++ rplugin/python3/neovim-spotify/__init__.py
@@ -2,8 +2,6 @@ from pynvim.api.buffer import Buffer
 from pynvim.api.nvim import Nvim
 from pynvim.api.window import Window
 from typing import Dict, Union
-from .ui import  create_anchor, create_placeholder
-from .actions import fetch_currently_playing_track, create_input
 import pynvim
 import logging
 
@@ -19,21 +17,12 @@ class NeovimSpotify:
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
 
-
-    def start(self) -> None:
-        self.config_plugin()
-
-        create_placeholder(self)
-        fetch_currently_playing_track(self)
-        create_input(self)
-
     def config_plugin(self) -> None:
 
         self.logger.info("Configuring plugin")
         self.nvim.command("hi SpotifyBorder guifg=#1db954")
         self.nvim.command("hi SpotifyText guifg=#1ed760")
         self.nvim.command("hi SpotifySelection guifg=#191414 guibg=#1ed760")
-        create_anchor(self)
 
     @pynvim.command("Spotify", sync=True)
     def hello_world(self):

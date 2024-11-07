fcdeffa7f1e9f46268ba11335796a3fb8517257d
AvivKermann <avivkermann@gmail.com>
2024-11-07 17:46:41 +0200

    changes

--- rplugin/python3/neovim-spotify/__init__.py
+++ rplugin/python3/neovim-spotify/__init__.py
@@ -2,12 +2,18 @@ from pynvim.api.buffer import Buffer
 from pynvim.api.nvim import Nvim
 from pynvim.api.window import Window
 from typing import Dict, Union
+from .ui import  create_anchor, create_placeholder
+from .actions import fetch_currently_playing_track, create_input
 import pynvim
 import logging
 
 
 @pynvim.plugin
 class NeovimSpotify:
+
+    HEIGHT= 48
+    WIDTH= 3
+
     def __init__(self, nvim: Nvim):
         self.nvim = nvim
         self.buffer: Union[None,Buffer] = None
@@ -17,6 +23,25 @@ class NeovimSpotify:
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
 
+
+    def start(self) -> None:
+        self.config_plugin()
+
+        create_placeholder(self)
+        fetch_currently_playing_track(self)
+        create_input(self)
+
+    @pynvim.function("SpotifyConfig", sync=True)
+    def config_plugin(self) -> None:
+
+        self.logger.info("Configuring plugin")
+        self.nvim.command("hi SpotifyBorder guifg=#1db954")
+        self.nvim.command("hi SpotifyText guifg=#1ed760")
+        self.nvim.command("hi SpotifySelection guifg=#191414 guibg=#1ed760")
+        create_anchor(self)
+
     @pynvim.command("Spotify", sync=True)
     def hello_world(self):
         self.nvim.command("echo 'Hello, world!'")
+
+

c8fef9ff09649133f535cc2b0537de92767212a5
AvivKermann <avivkermann@gmail.com>
2024-11-09 11:43:45 +0200

    checking

--- lua/neovim-spotify.lua
+++ lua/neovim-spotify.lua
@@ -1,19 +1,39 @@
-local builtins = require("telescope.builtin")
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local conf = require('telescope.config').values
 
 local M = {}
 
 function M.search_tracks()
-    builtins.input({
-        prompt = "Search Tracks",
-        default_text = "",
-        on_submit = function(query)
-            if query and query ~= "" then
-                vim.cmd("SpotifySearch " .. vim.fn.escape(query, " "))
-            else
-                print("Search query cannot be empty")
-            end
+    pickers.new({}, {
+        prompt_title = 'Search Spotify Tracks',
+        finder = finders.new_table {
+            results = {},
+            entry_maker = function(entry)
+                return {
+                    value = entry,
+                    display = entry,
+                    ordinal = entry,
+                }
+            end,
+        },
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                local input = action_state.get_current_line()
+                actions.close(prompt_bufnr)
+                if input and input ~= "" then
+                    vim.cmd("SpotifySearch " .. vim.fn.escape(input, " "))
+                else
+                    print("Search query cannot be empty")
+                end
+            end)
+            return true
         end,
-    })
+    }):find()
 end
 
 return M
+

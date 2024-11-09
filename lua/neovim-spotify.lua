ea3ecac583300fe0b4b492dc72bc089d3729313c
AvivKermann <avivkermann@gmail.com>
2024-11-09 12:01:12 +0200

    maybe now?

--- lua/neovim-spotify.lua
+++ lua/neovim-spotify.lua
@@ -9,16 +9,6 @@ local M = {}
 function M.search_tracks()
     pickers.new({}, {
         prompt_title = 'Search Spotify',
-        finder = finders.new_table {
-            results = {}, -- No initial results, just an input prompt.
-            entry_maker = function(entry)
-                return {
-                    value = entry,
-                    display = entry,
-                    ordinal = entry,
-                }
-            end,
-        },
         sorter = conf.generic_sorter({}),
         layout_config = {
             width = 0.5, -- Adjust width of the picker window (50% of the screen width)

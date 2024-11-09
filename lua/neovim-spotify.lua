f5fd430552a3a8fc2034975f3dff3bfc19163299
AvivKermann <avivkermann@gmail.com>
2024-11-09 17:39:27 +0200

    changes now showing hopefully results in telescope interface

--- lua/neovim-spotify.lua
+++ lua/neovim-spotify.lua
@@ -5,19 +5,35 @@ local actions_state = require "telescope.actions.state"
 local entry_display = require "telescope.pickers.entry_display"
 local conf = require("telescope.config").values
 
+-- local tracks = {
+--   { artist = "Metallica", title = "Nothing Else Matters", uri = "spotify:track:0nLiqZ6A27jJri2VCalIUs" },
+--   { artist = "Metallica", title = "Nothing Else Matters - Remastered 2021", uri = "spotify:track:6QAsrXPnMSXIbV0yEJHlEX" },
+--   { artist = "Metallica", title = "Nothing Else Matters - Live", uri = "spotify:track:4nvFgshg9sO9YVnlKgQysN" },
+--   { artist = "Metallica", title = "Nothing Else Matters - Live", uri = "spotify:track:5jEfQZmmwwKymr9cfmgfsd" },
+--   { artist = "Miley Cyrus", title = "Nothing Else Matters", uri = "spotify:track:2mC33mxgLJN3IT65MGN3B0" },
+--   { artist = "Hardwell", title = "Nothing Else Matters vs F*CKING SOCIETY - Hardwell Mashup", uri = "spotify:track:107nvz0Fjnsq9O9g61myZ5" },
+--   { artist = "Naughty Boy", title = "Runnin' (Lose It All)", uri = "spotify:track:5yZvaUVyuXfSVUaMumFi6l" },
+--   { artist = "newvoicesbg", title = "Nothing Else Metters", uri = "spotify:track:5YEqUrOkj1Pvw3mCXVALyQ" },
+--   { artist = "Roni Alter", title = "I Follow Rivers", uri = "spotify:track:4a2MIdV2CBdA9mUOgi0RWR" },
+--   { artist = "Yoni Rechter", title = "שוב היא כאן", uri = "spotify:track:0G8ooolx3x5DNkKkcZ19zp" }
+-- }
+
+-- Finder function for tracks
 local function finder_fn()
     return function(_)
-        local res = vim.g.spotify_search
+        local res = vim.g.spotify_search_results
         local results = {}
 
         for _, v in pairs(res) do
-            table.insert(results, { v[1], v[2], v[3] })
+            print("Track found:", v.artist, v.title)  -- Debugging line
+            table.insert(results, { artist = v.artist, title = v.title, uri = v.uri })
         end
 
         return results
     end
 end
 
+-- Entry maker function for handling track entries in Telescope
 local function entry_fn(opts)
     opts = opts or {}
 
@@ -32,12 +48,6 @@ local function entry_fn(opts)
     }
 
     local make_display = function (entry)
-        if vim.g.spotify_type == 'Artists' or vim.g.spotify_type == 'Playlists' then
-            return displayer {
-                { entry.track, "TelescopeResultsNumber" },
-            }
-        end
-
         return displayer {
             { entry.track, "TelescopeResultsNumber" },
             { entry.artist, "TelescopeResultsComment" },
@@ -45,21 +55,21 @@ local function entry_fn(opts)
     end
 
     return function(entry)
-        print(vim.inspect(entry))
         return {
-            artist = entry[2],
-            track = entry[1],
-            uri = entry[3],
+            artist = entry.artist,
+            track = entry.title,
+            uri = entry.uri,
             display = make_display,
-            ordinal = entry[1] .. entry[2],
+            ordinal = entry.title .. " " .. entry.artist,  -- using title and artist for sorting
         }
     end
 end
 
+-- Main function for showing tracks using Telescope
 local spotify = function (opts)
     opts = opts or {}
     pickers.new(opts, {
-        prompt_title = "Spotify (" .. vim.g.spotify_type .. ": " .. vim.g.spotify_title .. ")",
+        prompt_title = "Spotify Search Results",
         finder = finders.new_dynamic({
             entry_maker = entry_fn(opts),
             fn = finder_fn()
@@ -77,9 +87,93 @@ local spotify = function (opts)
     }):find()
 end
 
-local M = {}
-M.namespace = "Spotify"
-function M.init()
-    spotify(require("telescope.themes").get_dropdown{})
+-- Function to list available Spotify devices
+local list_devices = function (opts)
+    opts = opts or {}
+    pickers.new(opts, {
+        prompt_title = "Connect to a Device",
+        finder = finders.new_dynamic({
+            entry_maker = function (entry)
+                return {
+                    value = entry,
+                    display = entry[1],
+                    ordinal = entry[1]
+                }
+            end,
+            fn =  function(_)
+                local res = vim.g.spotify_devices
+                local results = {}
+
+                for _, v in pairs(res) do
+                    table.insert(results, { v[1] })
+                end
+
+                return results
+            end
+        }),
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function (prompt_bufnr, _)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = actions_state.get_selected_entry()
+                vim.g.spotify_device = selection.value
+            end)
+            return true
+        end
+    }):find()
 end
+
+local M = {
+    opts = {
+        status = {
+            update_interval = 10000,
+            format = '%s %t by %a'
+        }
+    },
+    status = {},
+    _status_line = ""
+}
+
+M.namespace = 'Spotify'
+
+-- Setup function to configure mappings
+function M.setup(opts)
+    M.opts = vim.tbl_deep_extend("force", M.opts, opts)
+
+    vim.api.nvim_set_keymap("n", "<Plug>(SpotifySkip)", ":call SpotifyPlayback('next')<CR>", { silent = true })
+    vim.api.nvim_set_keymap("n", "<Plug>(SpotifyPause)", ":call SpotifyPlayback('pause')<CR>", { silent = true })
+    vim.api.nvim_set_keymap("n", "<Plug>(SpotifySave)", ":call SpotifySave()<CR>", { silent = true })
+    vim.api.nvim_set_keymap("n", "<Plug>(SpotifyPrev)", ":call SpotifyPlayback('prev')<CR>", { silent = true })
+    vim.api.nvim_set_keymap("n", "<Plug>(SpotifyShuffle)", ":call SpotifyPlayback('shuffle')<CR>", { silent = true })
+end
+
+function M.init()
+    local opts = require'telescope.themes'.get_dropdown{}
+    spotify(opts)
+end
+
+function M.devices()
+    list_devices(require'telescope.themes'.get_dropdown{})
+end
+
+-- Status management for Spotify
+function M.status:start()
+    local timer = vim.loop.new_timer()
+    timer:start(1000, M.opts.status.update_interval, vim.schedule_wrap(function ()
+        local cmd = "spt playback --status --format '" .. M.opts.status.format .. "'"
+        vim.fn.jobstart(cmd, { on_stdout = self.on_event, stdout_buffered = true })
+    end))
+end
+
+function M.status:on_event(data)
+    if data then
+        M._status_line = data[1]
+    end
+end
+
+function M.status:listen()
+    return M._status_line
+end
+
+return M
 

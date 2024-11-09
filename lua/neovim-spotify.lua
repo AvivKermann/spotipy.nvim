499ee0c554b96209ebf0a58ce579951cd967aaf7
AvivKermann <avivkermann@gmail.com>
2024-11-09 12:02:22 +0200

    changes

--- lua/neovim-spotify.lua
+++ lua/neovim-spotify.lua
@@ -1,5 +1,4 @@
 local pickers = require('telescope.pickers')
-local finders = require('telescope.finders')
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
 local conf = require('telescope.config').values
@@ -9,13 +8,22 @@ local M = {}
 function M.search_tracks()
     pickers.new({}, {
         prompt_title = 'Search Spotify',
-        sorter = conf.generic_sorter({}),
-        layout_config = {
-            width = 0.5, -- Adjust width of the picker window (50% of the screen width)
-            height = 0.1, -- Adjust height of the picker window (10% of the screen height)
-            preview_width = 0.4, -- Optional: Set the preview window width, you can remove this if not needed
+        finder = require('telescope.finders').new_table {
+            results = {}, -- Empty results to avoid displaying anything
+            entry_maker = function(entry)
+                return {
+                    value = entry,
+                    display = entry,
+                    ordinal = entry,
+                }
+            end,
         },
-        previewer = false,
+        sorter = conf.generic_sorter({}),
+        previewer = false, -- Disable the preview window
+        layout_config = {
+            width = 0.5, -- Adjust the width of the input box (50% of screen width)
+            height = 0.1, -- Adjust the height of the input box (10% of screen height)
+        },
         attach_mappings = function(prompt_bufnr, map)
             actions.select_default:replace(function()
                 local input = action_state.get_current_line()

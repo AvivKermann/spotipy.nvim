b25123e51a17fc883b5862fe304aa2aa96dd23bb
AvivKermann <avivkermann@gmail.com>
2024-11-09 11:55:18 +0200

    changes

--- lua/neovim-spotify.lua
+++ lua/neovim-spotify.lua
@@ -10,7 +10,7 @@ function M.search_tracks()
     pickers.new({}, {
         prompt_title = 'Search Spotify',
         finder = finders.new_table {
-            results = {}, -- No initial results; this is for input.
+            results = {}, -- No initial results, just an input prompt.
             entry_maker = function(entry)
                 return {
                     value = entry,
@@ -21,22 +21,16 @@ function M.search_tracks()
         },
         sorter = conf.generic_sorter({}),
         attach_mappings = function(prompt_bufnr, map)
-            local execute_search = function()
+            actions.select_default:replace(function()
                 local input = action_state.get_current_line()
                 actions.close(prompt_bufnr)
                 if input and input ~= "" then
+                    -- Execute the SpotifySearch command with the user input
                     vim.cmd("SpotifySearch " .. vim.fn.escape(input, " "))
                 else
                     print("Search query cannot be empty")
                 end
-            end
-
-            -- Map <CR> in Normal mode
-            map('n', '<CR>', execute_search)
-
-            -- Map <CR> in Insert mode
-            map('i', '<CR>', execute_search)
-
+            end)
             return true
         end,
     }):find()

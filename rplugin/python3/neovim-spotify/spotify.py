490c2982d4c55edbf0b1506148fbb0b417e2041e
AvivKermann <avivkermann@gmail.com>
2024-11-10 18:36:56 +0200

    changes

--- rplugin/python3/neovim-spotify/spotify.py
+++ rplugin/python3/neovim-spotify/spotify.py
@@ -38,8 +38,7 @@ class Spotify:
                 client_secret = os.getenv("CLIENT_SECRET"),
                 redirect_uri = os.getenv("REDIRECT_URI"),
                 scope = "user-modify-playback-state user-read-playback-state user-read-currently-playing user-library-read user-library-modify",
-                cache_path = self.cache,
-                open_browser = False
+                cache_path = self.cache
             ))
         except Exception as e:
             self.logger.critical(f"Error initializing Spotify client: {e}")

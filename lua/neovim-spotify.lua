629a48a72d977dbf6ba3613e00cbdf267f68681f
AvivKermann <avivkermann@gmail.com>
2024-11-17 19:10:59 +0200

    changes

--- lua/neovim-spotify.lua
+++ lua/neovim-spotify.lua
@@ -141,7 +141,7 @@ function M.setup(opts)
     vim.api.nvim_set_keymap("n", "<leader>mn", ":SpotifyPlayback -n<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<leader>mp", ":SpotifyPlayback -p<CR>", { noremap = true, silent = true })
     vim.api.nvim_set_keymap("n", "<leader>md", ":SpotifyDevices<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>ms", ":lua require('Spotipy.nvim').search()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>ms", ":lua require('neovim-spotify').search()<CR>", { noremap = true, silent = true })
 
 end
 
